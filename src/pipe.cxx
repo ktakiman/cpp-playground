46d207bcee5cc50fbe96efb264317c2244a171ff
Kei Takahashi <ktakahashi.dev@gmail.com>
2023-12-20 17:34:04 -0500

    missing file

--- src/pipe.cxx
+++ src/pipe.cxx
@@ -1,14 +1,23 @@
 #include "pipe.h"
 
 #include <iostream>
+#include <string>
 
 namespace Playground {
 
 void TestPipe() {
+  std::cin.unsetf(std::ios_base::skipws);
+  // char c;
+  // while (std::cin >> c) {
+  //   std::cout << c;
+  // }
   std::string s;
-  while (std::cin >> s) {
-    std::cout << "GOT: " << s << std::endl;
+  while (std::getline(std::cin, s)) {
+    std::cout << s;
   }
+  // while (std::cin >> s) {
+  //   std::cout << s;
+  // }
 }
 
 };  // namespace Playground
